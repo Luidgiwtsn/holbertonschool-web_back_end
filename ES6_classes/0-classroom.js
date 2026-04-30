871414aad1e8aab0c05f7ba452d75ccfbc2f569f
luidgiwtsn <lwbordeaux@gmail.com>
2026-04-30 11:49:03 +0200

    Implement ClassRoom class

---
+++ ES6_classes/0-classroom.js
@@ -0,0 +1,11 @@
+/**
+ * Représente une salle de classe.
+ */
+export default class ClassRoom {
+  /**
+   * @param {Number} maxStudentsSize - La capacité maximale d'élèves.
+   */
+  constructor(maxStudentsSize) {
+    this._maxStudentsSize = maxStudentsSize;
+  }
+}
