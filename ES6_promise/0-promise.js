0a8110b5ca9c6709bf6758f3cb618aaf11ff1de7
luidgiwtsn <lwbordeaux@gmail.com>
2026-05-06 11:51:47 +0200

    chore: configuration du projet ES6 et implémentation de la tâche 0

---
+++ ES6_promise/0-promise.js
@@ -0,0 +1,6 @@
+export default function getResponseFromAPI() {
+  return new Promise((resolve, reject) => {
+    /* Le corps de la promesse reste vide ici car 
+       l'exercice demande simplement de retourner une instance */
+  });
+}
